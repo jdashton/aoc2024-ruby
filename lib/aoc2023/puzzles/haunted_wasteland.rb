cfff0ccacfab02fd9af16d9b20afd4499b3caf4d
J. Daniel Ashton <jdashton@ashtonfam.org>
2023-12-11 20:34:35 -0500

    Add HauntedWasteland puzzle implementation and tests.
    Added code for the HauntedWasteland puzzle solution, handling directions and node movements. Accompanied by relevant tests, the solution calculates the steps required from various nodes to target nodes, taking into account the direction instructions and node connections defined in the input map.

---
+++ lib/aoc2023/puzzles/haunted_wasteland.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module AoC2023
+  module Puzzles
+    # For Day 8, we're turning left and right.
+    class HauntedWasteland
+      def self.day08
+        map = File.open('input/day08.txt') { |file| new file }
+        puts "Day  8, Part One: #{ map.part_one_directions } steps are required to reach ZZZ."
+        # puts "Day  8, Part Two: #{ map.follow_directions } steps are required to reach ZZZ."
+        puts
+      end
+
+      # AAA = (BBB, CCC)
+      PAT = /(\w\w\w) = \((\w\w\w), (\w\w\w)\)/
+
+      def initialize(file)
+        lines         = file.readlines(chomp: true)
+        @instructions = lines.first.chars.map { _1 == 'L' ? 0 : 1 }
+        @nodes        = lines[2..].to_h { |line| PAT.match(line) && [$1&.to_sym, [$2&.to_sym, $3&.to_sym]] }
+        # pp self
+      end
+
+      def part_one_directions = follow_directions(:AAA, Set[:ZZZ])
+
+      def follow_directions(node, end_nodes)
+        # puts "Finding num steps from #{ node } to #{ end_nodes }."
+        steps    = 0
+        inst_len = @instructions.length
+        until end_nodes.member?(node)
+          node  = @nodes[node][@instructions[steps % inst_len]]
+          steps += 1
+        end
+        steps
+      end
+
+      def part_two_directions
+        start_nodes = @nodes.keys.select { _1.end_with?(?A) }
+        end_nodes   = @nodes.keys.select { _1.end_with?(?Z) }.to_set
+        start_nodes.map { |node|
+          # print "#{ node } -> any Z node: "
+          follow_directions(node, end_nodes)
+        }.reduce(&:lcm)
+      end
+    end
+  end
+end
