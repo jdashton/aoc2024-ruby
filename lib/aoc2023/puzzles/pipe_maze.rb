7c6435d3578403e4df2580a0690cc3c08ca4070a
J. Daniel Ashton <jdashton@ashtonfam.org>
2023-12-12 20:16:13 -0500

    Improve PipeMaze logic and augment tests
    The commit enhances PipeMaze class by adding methods for navigating through the maze, finding the start point, and connecting neighbors. Moreover, it includes a large set of test cases to ensure the new logic functions as expected.

--- lib/aoc2023/puzzles/pipe_maze.rb
+++ lib/aoc2023/puzzles/pipe_maze.rb
@@ -8,7 +8,7 @@ module AoC2023
         sketch = File.open('input/day10.txt') { |file| new file }
         puts "Day  10, Part One: #{ sketch.find_antipode } steps along the loop to get from the starting position " \
                'to the point farthest from the starting position.'
-        # puts "Day  10, Part Two: #{ $INSTANCE_NAME.total_winnings_with_joker } are the new total winnings."
+        puts "Day  10, Part Two: #{ sketch.find_points_in_loop } points are inside the loop."
         puts
       end
 
@@ -30,13 +30,23 @@ module AoC2023
       NEIGHBOR_WEST  = Set[?F, ?L, ?-]
       NEIGHBOR_EAST  = Set[?7, ?J, ?-]
 
-      def find_connecting_neighbors(x, y)
+      def find_connecting_neighbors((x, y))
         connectors = []
         connectors << [x, y - 1] if NEIGHBOR_NORTH.member?(north_neighbor(x, y))
         connectors << [x, y + 1] if NEIGHBOR_SOUTH.member?(south_neighbor(x, y))
         connectors << [x - 1, y] if NEIGHBOR_WEST.member?(west_neighbor(x, y))
         connectors << [x + 1, y] if NEIGHBOR_EAST.member?(east_neighbor(x, y))
         connectors
+      end
+
+      def replace_start
+        char_sets = []
+        char_sets << NEIGHBOR_SOUTH if NEIGHBOR_NORTH.member?(north_neighbor(*@start_node))
+        char_sets << NEIGHBOR_NORTH if NEIGHBOR_SOUTH.member?(south_neighbor(*@start_node))
+        char_sets << NEIGHBOR_EAST if NEIGHBOR_WEST.member?(west_neighbor(*@start_node))
+        char_sets << NEIGHBOR_WEST if NEIGHBOR_EAST.member?(east_neighbor(*@start_node))
+
+        char_sets.reduce(&:intersection).first
       end
 
       def find_new_neighbor((x, y))
@@ -69,13 +79,12 @@ module AoC2023
 
           steps += 1
 
-          node_a, node_b = [node_a, node_b]
-                             .map do |node|
+          node_a, node_b = [node_a, node_b].map do |node|
             @visited << node
-            find_new_neighbor(node)
-              .then { |neighbors| neighbors.reject { @visited.member? _1 } }.first
+            find_new_neighbor(node).then { |neighbors| neighbors.reject { @visited.member? _1 } }.first
           end
         end
+        @visited << node_a
         steps
       end
 
@@ -90,11 +99,56 @@ module AoC2023
         # 2. Detect which neighbors connect to S.
         # 3. Walk both neighbors until they are at the same place.
 
-        start_x, start_y = find_start
-        # puts "Start at #{ start_x }, #{ start_y }"
-        @visited << [start_x, start_y]
-        find_connecting_neighbors(start_x, start_y)
+        @start_node = find_start
+        # puts "Start at #{ start_node }"
+        @visited << @start_node
+        find_connecting_neighbors(@start_node)
           .then { walk_pipe(_1, 1) }
+      end
+
+      def clear_non_loop_pipes
+        @lines.each_with_index do |line, y|
+          line.chars.each_with_index do |_, x|
+            @lines[y][x] = '.' unless @visited.member? [x, y]
+          end
+        end
+      end
+
+      def check_each_point
+        inside_points = 0
+        @lines.each do |line|
+          line.chars.each_with_index do |char, x|
+            next unless char == '.'
+
+            # puts "Checking (#{ x }, #{ y })"
+            # Count the number of pipes (F, 7 or |) to the nearest left or right edge.
+            # If odd, this point is inside the loop.
+            # If even, this point is outside the loop.
+            inside_points += 1 if line.chars[..x].count { |c| NEIGHBOR_NORTH.member? c }.odd?
+          end
+        end
+        inside_points
+      end
+
+      def find_points_in_loop
+        # First, run Part 1 so that the @visited Set gets populated.
+        find_antipode
+        # pp @lines
+
+        # Then, fix the map by replacing `S` with its proper character.
+        @lines[@start_node[1]][@start_node[0]] = replace_start
+
+        # Next, for each point in the map, if not in @visited, replace
+        # it with `.`.
+        clear_non_loop_pipes
+        # pp @lines
+
+        # Now that the map is ready,
+        #   for each point `.` in the map,
+        #   look left or right (whichever is nearer, rounding up)
+        # Count an intersection with `F`, with `7`, and with `|`.
+        # If odd, this point is inside the loop.
+        check_each_point
       end
     end
   end
--- spec/aoc2023/puzzles/pipe_maze_spec.rb
+++ spec/aoc2023/puzzles/pipe_maze_spec.rb
@@ -38,8 +38,86 @@ RSpec.describe AoC2023::Puzzles::PipeMaze do
       expect(sketch.find_antipode).to eq 6_951
     end
 
-    #   it 'finds 13,524,038,372,771 as the number of steps from all start nodes to all end nodes' do
-    #     expect(sketch.part_two_directions).to eq 13_524_038_372_771
-    #   end
+    it 'finds 563 as the number of steps from start to antipode' do
+      expect(sketch.find_points_in_loop).to eq 563
+    end
+  end
+
+  # noinspection SpellCheckingInspection
+  context 'with Part 2 first example' do
+    subject(:sketch) { described_class.new StringIO.new(<<~DATA) }
+      ...........
+      .S-------7.
+      .|F-----7|.
+      .||.....||.
+      .||.....||.
+      .|L-7.F-J|.
+      .|..|.|..|.
+      .L--J.L--J.
+      ...........
+    DATA
+
+    it 'finds 4 as the number of points inside the loop' do
+      expect(sketch.find_points_in_loop).to eq 4
+    end
+  end
+
+  # noinspection SpellCheckingInspection
+  context 'with Part 2 second example' do
+    subject(:sketch) { described_class.new StringIO.new(<<~DATA) }
+      ..........
+      .S------7.
+      .|F----7|.
+      .||OOOO||.
+      .||OOOO||.
+      .|L-7F-J|.
+      .|II||II|.
+      .L--JL--J.
+      ..........
+    DATA
+
+    it 'finds 4 as the number of points inside the loop' do
+      expect(sketch.find_points_in_loop).to eq 4
+    end
+  end
+
+  # noinspection SpellCheckingInspection
+  context 'with Part 2 third example' do
+    subject(:sketch) { described_class.new StringIO.new(<<~DATA) }
+      .F----7F7F7F7F-7....
+      .|F--7||||||||FJ....
+      .||.FJ||||||||L7....
+      FJL7L7LJLJ||LJ.L-7..
+      L--J.L7...LJS7F-7L7.
+      ....F-J..F7FJ|L7L7L7
+      ....L7.F7||L7|.L7L7|
+      .....|FJLJ|FJ|F7|.LJ
+      ....FJL-7.||.||||...
+      ....L---J.LJ.LJLJ...
+    DATA
+
+    it 'finds 8 as the number of points inside the loop' do
+      expect(sketch.find_points_in_loop).to eq 8
+    end
+  end
+
+  # noinspection SpellCheckingInspection
+  context 'with Part 2 fourth example' do
+    subject(:sketch) { described_class.new StringIO.new(<<~DATA) }
+      FF7FSF7F7F7F7F7F---7
+      L|LJ||||||||||||F--J
+      FL-7LJLJ||||||LJL-77
+      F--JF--7||LJLJ7F7FJ-
+      L---JF-JLJ.||-FJLJJ7
+      |F|F-JF---7F7-L7L|7|
+      |FFJF7L7F-JF7|JL---7
+      7-L-JL7||F7|L7F-7F7|
+      L.L7LFJ|||||FJL7||LJ
+      L7JLJL-JLJLJL--JLJ.L
+    DATA
+
+    it 'finds 10 as the number of points inside the loop' do
+      expect(sketch.find_points_in_loop).to eq 10
+    end
   end
 end
