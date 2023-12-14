0030bfa9c62599913ea82f06934592795d00c8ab
J. Daniel Ashton <jdashton@ashtonfam.org>
2023-12-14 17:50:05 -0500

    Implement PointOfIncidence class and tests
    The PointOfIncidence class was created with methods to solve the Advent of Code 2023 puzzle for Day 13. Corresponding tests were also added to ensure the correct behavior of the newly implemented functionality. The class and tests are primarily focused on finding and processing mirror reflections.

---
+++ spec/aoc2023/puzzles/point_of_incidence_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe AoC2023::Puzzles::PointOfIncidence do
+  # noinspection SpellCheckingInspection
+  context 'with provided test data' do
+    subject(:mirror_map) { described_class.new StringIO.new(<<~DATA) }
+      #.##..##.
+      ..#.##.#.
+      ##......#
+      ##......#
+      ..#.##.#.
+      ..##..##.
+      #.#.##.#.
+
+      #...##..#
+      #....#..#
+      ..##..###
+      #####.##.
+      #####.##.
+      ..##..###
+      #....#..#
+    DATA
+
+    it 'finds 405 as the summary of my notes' do
+      expect(mirror_map.find_reflections).to eq 405
+    end
+  end
+
+  context 'with actual input data' do
+    subject(:mirror_map) { File.open('input/day13.txt') { |file| described_class.new file } }
+
+    it 'finds 36,015 as the summary of my notes' do
+      expect(mirror_map.find_reflections).to eq 36_015
+    end
+  end
+end
