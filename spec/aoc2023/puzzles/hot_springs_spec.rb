d5052eba5f10b5588e46499cb6949fc9c1b29993
J. Daniel Ashton <jdashton@ashtonfam.org>
2023-12-14 16:22:10 -0500

    Add unfold option to find_arrangements method in hot_springs.rb
    The find_arrangements_for method in the HotSprings puzzle solution of AoC 2023 was updated to support an unfold option. Now, the method allows patterns to be repeated multiple times incrementally. This addition provides a framework for exploring more complex puzzle permutations.

--- spec/aoc2023/puzzles/hot_springs_spec.rb
+++ spec/aoc2023/puzzles/hot_springs_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe AoC2023::Puzzles::HotSprings do
     it 'finds 21 as the sum of possible arrangements' do
       expect(report.find_arrangements).to eq 21
     end
+
+    # it 'finds 525,152 as the sum of possible unfolded arrangements' do
+    #   expect(report.find_unfolded_arrangements).to eq 525_152
+    # end
   end
 
   describe '#find_arrangements_for' do
@@ -31,6 +35,15 @@ RSpec.describe AoC2023::Puzzles::HotSprings do
       expect(report.find_arrangements_for('????.######..#####. 1,6,5')).to eq 4
       expect(report.find_arrangements_for('?###???????? 3,2,1')).to eq 10
     end
+
+    # it 'finds the expected number of arrangements for a given unfolded pattern', :aggregate_failures do
+    # expect(report.find_arrangements_for('???.### 1,1,3', unfold: true)).to eq 1
+    # expect(report.find_arrangements_for('.??..??...?##. 1,1,3')).to eq 16_384
+    # expect(report.find_arrangements_for('?#?#?#?#?#?#?#? 1,3,1,6')).to eq 1
+    # expect(report.find_arrangements_for('????.#...#... 4,1,1')).to eq 16
+    # expect(report.find_arrangements_for('????.######..#####. 1,6,5')).to eq 2500
+    # expect(report.find_arrangements_for('?###???????? 3,2,1')).to eq 506_250
+    # end
   end
   # rubocop:enable RSpec/ExampleLength
 
