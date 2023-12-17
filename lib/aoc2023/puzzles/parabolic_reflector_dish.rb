ffa2bda4b66105f788a81696a50f743cc7a4e4e6
J. Daniel Ashton <jdashton@ashtonfam.org>
2023-12-16 20:18:10 -0500

    pre-refactoring

--- lib/aoc2023/puzzles/parabolic_reflector_dish.rb
+++ lib/aoc2023/puzzles/parabolic_reflector_dish.rb
@@ -12,11 +12,92 @@ module AoC2023
       end
 
       def initialize(file)
-        @lines = file.readlines(chomp: true)
+        @platform = file.readlines(chomp: true).map(&:chars)
+      end
+
+      def roll_left(platform)
+        platform.map do |row|
+          row
+            .join
+            .split(?#, -1)
+            .map { |segment| "#{ ?O * segment.count(?O) }#{ ?. * segment.count(?.) }" }
+            .join(?#)
+            .chars
+        end
+      end
+
+      def tilt(direction)
+        adjusted_platform = adjust_platform_before_roll(@platform, direction)
+        rolled_platform   = roll_left(adjusted_platform)
+        adjust_platform_after_roll(rolled_platform, direction)
+      end
+
+      def adjust_platform_before_roll(platform, direction)
+        # noinspection RubyCaseWithoutElseBlockInspection
+        case direction
+          when :west then platform
+          when :east then platform.map(&:reverse)
+          when :north then platform.transpose
+          when :south then platform.transpose.map(&:reverse)
+        end
+      end
+
+      def adjust_platform_after_roll(rolled_platform, direction)
+        # noinspection RubyCaseWithoutElseBlockInspection
+        case direction
+          when :west then rolled_platform
+          when :east then rolled_platform.map(&:reverse)
+          when :north then rolled_platform.transpose
+          when :south then rolled_platform.map(&:reverse).transpose
+        end
+      end
+
+      def cycle(count)
+        cache = {}
+        start = repeat = cycle_length = 0
+        (1..count).each do |i|
+          @platform = tilt(:north)
+          @platform = tilt(:west)
+          @platform = tilt(:south)
+          @platform = tilt(:east)
+          if cache[@platform]
+            # puts "Cycle #{ i } is the same as cycle #{ cache[@platform] }"
+            start        = cache[@platform]
+            repeat       = i
+            cycle_length = repeat - start
+            break
+          else
+            cache[@platform] = i
+          end
+        end
+        return @platform if start.zero?
+
+        inv_cache = cache.invert
+        # pp [start, repeat, cycle_length]
+
+        # pp((start...repeat).find { |i| i % cycle_length == count % cycle_length })
+
+        # (start...repeat).each { |i| pp calculate_load(inv_cache[i]) }
+
+        inv_cache[(start...repeat).find { |i| i % cycle_length == count % cycle_length }]
+      end
+
+      def calculate_load(frame = @platform)
+        frame
+          .reverse
+          .map
+          .with_index(1) { |row, index| index * row.count(?O) }
+          .sum
       end
 
       def total_load_on_beams
-        1
+        @platform = tilt(:north)
+        calculate_load
+      end
+
+      def total_load_on_beams_after_billion
+        frame = cycle(1_000_000_000)
+        calculate_load frame
       end
     end
   end
--- spec/aoc2023/puzzles/parabolic_reflector_dish_spec.rb
+++ spec/aoc2023/puzzles/parabolic_reflector_dish_spec.rb
@@ -1,5 +1,44 @@
 # frozen_string_literal: true
 
+ONE_CYCLE = <<~DATA.chomp
+  .....#....
+  ....#...O#
+  ...OO##...
+  .OO#......
+  .....OOO#.
+  .O#...O#.#
+  ....O#....
+  ......OOOO
+  #...O###..
+  #..OO#....
+DATA
+
+TWO_CYCLES = <<~DATA.chomp
+  .....#....
+  ....#...O#
+  .....##...
+  ..O#......
+  .....OOO#.
+  .O#...O#.#
+  ....O#...O
+  .......OOO
+  #..OO###..
+  #.OOO#...O
+DATA
+
+THREE_CYCLES = <<~DATA.chomp
+  .....#....
+  ....#...O#
+  .....##...
+  ..O#......
+  .....OOO#.
+  .O#...O#.#
+  ....O#...O
+  .......OOO
+  #...O###.O
+  #.OOO#...O
+DATA
+
 RSpec.describe AoC2023::Puzzles::ParabolicReflectorDish do
   # noinspection SpellCheckingInspection
   context 'with provided test data' do
@@ -17,17 +56,35 @@ RSpec.describe AoC2023::Puzzles::ParabolicReflectorDish do
     DATA
 
     it 'finds 136 as the total load on the north support beams' do
-      pending('Implement this')
       expect(mirror_map.total_load_on_beams).to eq 136
+    end
+
+    it 'finds the expect configuration after 1 cycle' do
+      expect(mirror_map.cycle(1).map(&:join).join("\n")).to eq(ONE_CYCLE)
+    end
+
+    it 'finds the expect configuration after 2 cycles' do
+      expect(mirror_map.cycle(2).map(&:join).join("\n")).to eq(TWO_CYCLES)
+    end
+
+    it 'finds the expect configuration after 3 cycles' do
+      expect(mirror_map.cycle(3).map(&:join).join("\n")).to eq(THREE_CYCLES)
+    end
+
+    it 'finds 64 as the total load on the north support beams after ONE BILLION cycles' do
+      expect(mirror_map.total_load_on_beams_after_billion).to eq 64
     end
   end
 
   context 'with actual input data' do
     subject(:mirror_map) { File.open('input/day14.txt') { |file| described_class.new file } }
 
-    it 'finds 136 as the total load on the north support beams' do
-      pending('Implement this')
-      expect(mirror_map.total_load_on_beams).to eq 136
+    it 'finds 109,596 as the total load on the north support beams' do
+      expect(mirror_map.total_load_on_beams).to eq 109_596
+    end
+
+    it 'finds 96,105 as the total load on the north support beams after ONE BILLION cycles' do
+      expect(mirror_map.total_load_on_beams_after_billion).to eq 96_105
     end
   end
 end
